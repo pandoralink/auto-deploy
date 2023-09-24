a3f264d50cf0793d3bf141160c6e34c0f0e0b79b
poplink <2219535001@qq.com>
2023-09-25 00:03:38 +0800

    feat: update user route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource 2');
+  res.send('respond with a resource 3');
 });
 
 module.exports = router;
