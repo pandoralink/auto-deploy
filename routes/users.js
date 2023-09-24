cb1c8a0a3156bad0945bd9c4715f361a52ac0537
poplink <2219535001@qq.com>
2023-09-24 19:37:57 +0800

    feat: test pm2 is execute

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource 1');
+  res.send('respond with a resource 2');
 });
 
 module.exports = router;
